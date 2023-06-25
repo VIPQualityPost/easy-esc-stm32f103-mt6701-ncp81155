d5f31aa93ef627f0a81b2f773941d37f1da2c28a
Dalton Scharff <dalton.scharff@gmail.com>
2023-06-24 21:56:53 -0400

    remove serialUsb from useMonitoring call

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -12,32 +12,34 @@
 /**
  * Magnetic sensor configuration schemes.
  * Set using build flag -DMT6701_ABZ, -DMT6701_I2C, -DMT6701_SSI.
-*/
+ */
 #ifdef MT6701_ABZ
 #define ENC_CPR 1024
 Encoder mt6701 = Encoder(ENC_A, ENC_B, ENC_CPR, ENC_Z);
 
 // interrupt handlers
-void doA(){
+void doA()
+{
   mt6701.handleA();
 }
 
-void doB(){
+void doB()
+{
   mt6701.handleB();
 }
 
-void doZ(){
+void doZ()
+{
   mt6701.handleIndex();
 }
 #endif
 
 #ifdef MT6701_I2C
 MT6701_I2CConfig_s mt6701_config = {
-  .chip_address = 0b0000110, 
-  .bit_resolution = 14, 
-  .angle_register=0x03, 
-  .data_start_bit= 8
-  }; 
+    .chip_address = 0b0000110,
+    .bit_resolution = 14,
+    .angle_register = 0x03,
+    .data_start_bit = 8};
 
 MT6701_Serial_I2C mt6701 = MT6701_Serial_I2C(mt6701_config);
 TwoWire enc_i2c(I2C2_SDA, I2C2_SCL);
@@ -48,59 +50,62 @@ TwoWire enc_i2c(I2C2_SDA, I2C2_SCL);
 #endif
 
 // Prepare SimpleFOC constructors.
-BLDCDriver3PWM driver =  BLDCDriver3PWM(PWM_U, PWM_V, PWM_W, EN_U, EN_V, EN_W);
-BLDCMotor motor = BLDCMotor(POLEPAIRS,Rphase,MOTOR_KV);
+BLDCDriver3PWM driver = BLDCDriver3PWM(PWM_U, PWM_V, PWM_W, EN_U, EN_V, EN_W);
+BLDCMotor motor = BLDCMotor(POLEPAIRS, Rphase, MOTOR_KV);
 // HardwareSerial stlinkSerial(UART1_RX,UART1_TX);
 // RTTStream rtt;
 
 #ifdef HAS_COMMANDER
-Commander commander = Commander(SerialUSB);
-void doMotor(char *cmd){
-  commander.motor(&motor,cmd);
+Commander commander = Commander();
+void doMotor(char *cmd)
+{
+  commander.motor(&motor, cmd);
 }
 #endif
 
-void mt6701_i2c_enable(bool state){
-  digitalWrite(ENC_MODE,state);
-  digitalWrite(ENC_I2C_EN,state);
+void mt6701_i2c_enable(bool state)
+{
+  digitalWrite(ENC_MODE, state);
+  digitalWrite(ENC_I2C_EN, state);
 }
 
-void setup(){
+void setup()
+{
 
-  #ifdef SIMPLEFOC_STM32_DEBUG
-  SimpleFOCDebug::enable(&SerialUSB);
-  #endif
+#ifdef SIMPLEFOC_STM32_DEBUG
+  SimpleFOCDebug::enable();
+#endif
 
-  pinMode(ENC_MODE,OUTPUT);
-  pinMode(ENC_I2C_EN,OUTPUT);
+  pinMode(ENC_MODE, OUTPUT);
+  pinMode(ENC_I2C_EN, OUTPUT);
 
-  #ifdef MT6701_ABZ
+#ifdef MT6701_ABZ
   // set the MT6701 to ABZ mode
   mt6701_i2c_enable(0);
   delay(1000);
   // Initialize after letting settle briefly (600ms?)
   mt6701.quadrature = Quadrature::OFF;
-  mt6701.init(); 
-  mt6701.enableInterrupts(doA,doB,doZ);
+  mt6701.init();
+  mt6701.enableInterrupts(doA, doB, doZ);
   motor.linkSensor(&mt6701);
-  #endif
+#endif
 
-  #ifdef MT6701_I2C
+#ifdef MT6701_I2C
   // set the MT6701 to serial mode
   mt6701_i2c_enable(1);
-  delay(1000); //let chip settle
+  delay(1000); // let chip settle
   mt6701.init(&enc_i2c);
   motor.linkSensor(&mt6701);
-  #endif
+#endif
 
-  #ifdef MT6701_SSI
-  // SSI init code
-  #endif
+#ifdef MT6701_SSI
+// SSI init code
+#endif
 
   // setup the driver
   driver.pwm_frequency = 25000;
   driver.voltage_power_supply = 5;
-  driver.voltage_limit  = 5;
+  driver.voltage_limit = 5;
   driver.init();
   motor.linkDriver(&driver);
 
@@ -115,7 +120,7 @@ void setup(){
   motor.P_angle.I = 10;
   // motor.P_angle.D = 0;
   // motor.P_angle.output_ramp = 1000; //rad/s^2
-  motor.LPF_angle.Tf = 0; //try to avoid
+  motor.LPF_angle.Tf = 0; // try to avoid
 
   // motor parameters
   motor.voltage_sensor_align = 2;
@@ -127,30 +132,29 @@ void setup(){
   motor.init();
   motor.initFOC();
 
-  // Commander actions
-  #ifdef HAS_COMMANDER
+// Commander actions
+#ifdef HAS_COMMANDER
   SerialUSB.begin();
-  motor.useMonitoring(SerialUSB);
+  motor.useMonitoring(Serial);
   motor.monitor_start_char = 'M';
   motor.monitor_end_char = 'M';
   motor.monitor_downsample = 250;
-  commander.add('M',doMotor,"motor");
+  commander.add('M', doMotor, "motor");
   commander.verbose = VerboseMode::machine_readable;
-  #endif
+#endif
 
   // rtt.println("setup done!");
   motor.target = 2;
-
 }
 
-void loop() {
+void loop()
+{
 
   motor.loopFOC();
   motor.move();
 
-  #ifdef HAS_COMMANDER
+#ifdef HAS_COMMANDER
   motor.monitor();
   commander.run();
-  #endif
-
+#endif
 }
